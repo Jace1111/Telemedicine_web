ae26e430280d7d39fd30ecdfeffbcaaba5daa177
Jace1111 <jose.a.acevedo01@utrv.edu>
2020-11-03 20:35:13 -0600

    2nd Commit. Created the visuals

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :patients
+  devise_for :accounts
+  root to: 'public#main'
   resources :measures
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20201104003711_create_measures.rb
+++ db/migrate/20201104003711_create_measures.rb
@@ -1,6 +1,6 @@
 class CreateMeasures < ActiveRecord::Migration[6.0]
   def change
-    create_table :measures do |t|
+    create_table :New Patient do |t|
       t.string :Patient_Name
       t.float :Date
       t.float :Time
